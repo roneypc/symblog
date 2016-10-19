7025659d9abc94966f0983f115fcb940b47d8770
Jorge Roberto Neyra <roneypc@gmail.com>
2016-10-19 20:43:06 +0200

    Doctrine introduction

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -4,6 +4,9 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Importa el nuevo espacio de nombres
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
 
 /**
 
@@ -17,4 +20,41 @@ class PageController extends Controller {
 	public function aboutAction() {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
 	}
+
+	public function contactAction() {
+		$enquiry = new Enquiry();
+		$form = $this->createForm(new EnquiryType(), $enquiry);
+
+		$request = $this->getRequest();
+		if ($request->getMethod() == 'POST') {
+			$form->handleRequest($request);
+
+			if ($form->isValid()) {
+				$transport = \Swift_SmtpTransport::newInstance()
+				            ->setUsername('roneypc@hotmail.com')->setPassword('R0b3rt05')
+				            ->setHost('smtp-mail.outlook.com')
+				            ->setPort(587)->setEncryption('tls');
+
+				$mailer = \Swift_Mailer::newInstance($transport);
+				        
+				$message = \Swift_Message::newInstance()
+				->setSubject('Contact enquiry from symblog')
+				->setFrom('roneypc@hotmail.com')
+				->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+            	->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));				
+				        
+				$result = $mailer->send($message);
+
+				// $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+				// Redirige - Esto es importante para prevenir que el usuario
+				// reenvíe el formulario si actualiza la página
+				return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+			}
+		}
+
+		return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+		'form' => $form->createView()
+		));
+    }
 }
